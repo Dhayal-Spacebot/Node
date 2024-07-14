de32b7c2f1fb418f6324a1518d0a21cac3e4f775
Dhayalan <dhayalantony@gmail.com>
2024-07-14 12:01:48 +0530

    register user with mongodb

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,12 +1,4 @@
-const usersDB = {
-    users: require('../data/users.json'),
-    setUsers: function(userData) {this.users = userData}
-};
-
-// const User = require('../data/User');
-
-const fsPromises = require('fs').promises;
-const path = require('path');
+const User = require('../data/User');
 const bcrypt = require('bcrypt');
 
 const handleNewUser = async(req, res) => {
@@ -15,27 +7,18 @@ const handleNewUser = async(req, res) => {
         return res.status(400).json({"Message": 'Username and password are required'});
 
     // check for the duplicate userName in the database
-    const duplicateUser = usersDB.users.find(person => person.userName === user);
-    // const duplicateUser = await User.findOne({username: user}).exec();
+    const duplicateUser = await User.findOne({username: user}).exec();
     if(duplicateUser)
-        return res.sendStatus(409);
         return res.status(409).json({"Message": "Username already exist in the database"})
 
         try{
             // encrypt the password
             const hashedPwd = await bcrypt.hash(pwd,10);
             // store the hashed password to db
-            const newUser = {
+            const newUser = await User.create({
                 "username": user,
-                "roles": {"user": 6791},
                  "password": hashedPwd
-                };
-            usersDB.setUsers([...usersDB.users, newUser]);
-            await fsPromises.writeFile(
-                 path.join(__dirname,'..','data','users.json'),
-                 JSON.stringify(usersDB.users)
-            );
-            console.log(usersDB.users);
+                });
             console.log(newUser);
             res.status(201).json({'success': `new user ${user} is created!`})
 
